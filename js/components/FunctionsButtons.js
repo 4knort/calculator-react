f9f9d5a73aef40368c400b406d66b6ce93eb9c7a
vadim <vadimc@saltedge.com>
2016-10-21 16:49:53 +0300

    FunctionsButtons refactored

--- js/components/FunctionsButtons.js
+++ js/components/FunctionsButtons.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 const FunctionsButtons = ({ onClick, buttonFunction }) => {
   const buttons = ['+', '-', '*', '/'];
-  const buttonObjects = buttons.map((item, i) => <button onClick= {() => onNumberClick(item)} key={i} className="button-number">{item}</button>)
-
-  const onNumberClick = (number) => onClick(number);
-  const onFunctionClick = (operation) => buttonFunction(operation);
+  const buttonObjects = buttons.map((item, i) => <button onClick= {() => onClick(item)} key={i} className="button-number">{item}</button>)
 
   return (
     <div>
       {buttonObjects}
-      <button onClick= {() => onFunctionClick("clear")} className="button-number">Clear</button>
-      <button onClick= {() => onFunctionClick("delete")} className="button-number">←</button>
-      <button onClick= {() => onFunctionClick("equal")} className="button-number">=</button>
+      <button onClick= {() => buttonFunction("clear")} className="button-number">Clear</button>
+      <button onClick= {() => buttonFunction("delete")} className="button-number">←</button>
+      <button onClick= {() => buttonFunction("equal")} className="button-number">=</button>
     </div>
   )
 }
